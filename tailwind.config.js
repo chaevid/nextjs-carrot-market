324923050bc53e10a1345248ee4423189b162ef8
chaevid <chaevid@gmail.com>
2023-02-09 18:34:57 +0900

    Customize Color & Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/contents/**/*.{js,jsx,ts,tsx}"
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/contents/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'Noto Sans KR'],
+      },
+      colors: {
+        'arws-gold': {
+          50: '#FFF9E1',
+          100: '#FEEFB2',
+          200: '#FEE480',
+          300: '#FFDB4A',
+          400: '#FFD118',
+          500: '#FFC800',
+          600: '#FFBA00',
+          700: '#FFA600',
+          800: '#FF9500',
+          900: '#FF7400',
+          DEFAULT: '#FFD118',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
