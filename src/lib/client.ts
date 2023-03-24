727248ebc13283f971e0c28a8f7cf0bc3a162e21
chaevid <chaevid@gmail.com>
2023-03-24 17:41:45 +0900

    #7.1 Making Forms Alone

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { PrismaClient } from '@prisma/client';
 // https://www.prisma.io/docs/concepts/components/prisma-client
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default new PrismaClient();
+// export default new PrismaClient();
