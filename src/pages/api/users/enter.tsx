2ecb4da9f3443ced371c835d782776dd52cf8845
chaevid <chaevid@gmail.com>
2023-04-09 23:11:57 +0900

    #9.4 Sending SMS

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import twilio from 'twilio';
 import crypto from 'crypto';
 import client from '@/lib/server/client';
 import withHandler, { ResponseType } from '@/lib/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -18,7 +21,7 @@ async function handler(
 
   const payload = crypto
     .createHash('sha256')
-    .update(user.email || user.phone)
+    .update(user.email || user.phone?.toString())
     .digest('hex');
   const token = await client.token.create({
     data: {
@@ -36,6 +39,15 @@ async function handler(
       },
     },
   });
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSG_SID,
+      to: process.env.MY_PHONE!, //PRODUCTION : `to: phone`
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
   return res.json({
     ok: true,
   });
