3a2addd45388ccb98aadb150b964dc7248dfdbd7
chaevid <chaevid@gmail.com>
2023-04-10 02:58:43 +0900

    #10.3 SWR

--- src/lib/client/useUser.ts
+++ src/lib/client/useUser.ts
@@ -1,18 +1,14 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR('/api/users/me', fetcher);
   const router = useRouter();
-  useEffect(() => {
-    fetch('/api/users/me')
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace('/enter');
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+
+  //          return router.replace('/enter');
+
+  return data;
 }
