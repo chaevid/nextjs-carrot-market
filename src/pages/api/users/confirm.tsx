62baeeafeca0cb915dfc8db95e696e213105cba3
chaevid <chaevid@gmail.com>
2023-04-10 01:06:07 +0900

    #9.7 Serverless Sessions - 'iron-session'
    https://github.com/vvo/iron-session

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
 import client from '@/lib/server/client';
 import withHandler, { ResponseType } from '@/lib/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -6,9 +7,23 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  // console.log(req.session);
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler('POST', handler);
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrotsession',
+  password:
+    'asdasdsadfasmfjkldsjhfkajklj213u90127391748932uo4912739821y39812yhajkdhsajkdnlkaf',
+});
